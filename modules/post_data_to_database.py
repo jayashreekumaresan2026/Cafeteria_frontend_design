fbf637ef1c2e7f26705d89ddfac5c86903ccad52
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-19 22:17:53 +0530

    added_for_employee_login

--- modules/post_data_to_database.py
+++ modules/post_data_to_database.py
@@ -1,6 +1,6 @@
 def post_data(connection, user_data):
     cursor = connection.cursor()
-    cursor.execute("""insert into employee(name, employee_id) values(%s, %s);""",
-                   (user_data['name'], user_data['id']))
+    cursor.execute("""insert into employee1(emp_id) values(%s);""",
+                   ( user_data['emp_id']))
     connection.commit()
     cursor.close()
