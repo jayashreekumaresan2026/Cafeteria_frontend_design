9d854207be5f128ab124db3c8e833f6fa5a4af0e
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-21 18:01:25 +0530

    checking_user_id_with_database

--- app.py
+++ app.py
@@ -11,17 +11,22 @@ connection = psycopg2.connect("dbname=thoughtworks_cafeteria user=admin")
 @app.route('/')
 def login_page():
     return render_template('creating_login_page.html')
-@app.route('/')
+
+
+@app.route('/customer_page')
 def customer_page():
     return render_template('creating_customer_login.html')
 
-@app.route('/menu_page')
-def menu_page():
-    return render_template('menu_page.html')
 
-@app.route('/employee-login')
+@app.route('/employee', methods=['POST'])
+def menu_page():
+    post_data(connection, request.form)
+    return render_template('menu_page.html', shared=request.form)
+
+
+@app.route('/beverage', methods=['GET'])
 def employee_page():
-    return render_template('creating_customer_login.html')
+    return render_template('.html')
 
 
 @app.route('/post-data', methods=['GET'])
@@ -30,12 +35,12 @@ def get_data():
     post_data(connection, request.form)
     return render_template('detail_page.html', shared=request.form)
 
+
 @app.route('/creating_customer_login', methods=['POST'])
 def post_data_request():
     print(request)
     post_data(connection, request.form)
     return render_template('detail_page.html', shared=request.form)
-
 
 
 if __name__ == '__main__':
