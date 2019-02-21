e84840791da6d0404a412501f2e2fe6c1bc7d8e0
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-21 23:22:26 +0530

    entering_vendor_details_with_database

--- modules/post_data_to_database.py
+++ modules/post_data_to_database.py
@@ -1,6 +1,19 @@
+
+#validating the user data with the database
+
 def post_data(connection, user_data):
+
     cursor = connection.cursor()
     cursor.execute("""select employee_id from employee_details where employee_id = %s ;""", (user_data['user_id'],))
-
     connection.commit()
     cursor.close()
+
+
+# validating the vendor data with the database
+def post_data_for_vendor(connection, vendor_data):
+    cursor = connection.cursor()
+    cursor.execute("""insert into employee(vendor_id, vendor_name,vendor_password) values(%s, %s, %s);""",
+                   (vendor_data['name'], vendor_data['id'],vendor_data['password']))
+    connection.commit()
+    cursor.close()
+
