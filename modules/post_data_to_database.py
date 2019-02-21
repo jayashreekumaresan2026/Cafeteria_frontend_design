c6f3fb02842339771cb9954107cb7ee9cdd7da19
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-21 18:00:20 +0530

    checking_user_id_with_database

--- modules/post_data_to_database.py
+++ modules/post_data_to_database.py
@@ -1,6 +1,6 @@
 def post_data(connection, user_data):
     cursor = connection.cursor()
-    cursor.execute("""insert into employee1(emp_id) values(%s);""",
-                   ( user_data['emp_id']))
+    cursor.execute("""select employee_id from employee_details where employee_id = %s ;""", (user_data['user_id'],))
+
     connection.commit()
     cursor.close()
