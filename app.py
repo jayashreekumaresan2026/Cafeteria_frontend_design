d07f21023e352304836e4687bf84f34e4362e86f
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-22 10:48:46 +0530

    added user data to the  database

--- app.py
+++ app.py
@@ -7,49 +7,57 @@ app = Flask(__name__)
 CORS(app)
 connection = psycopg2.connect("dbname=thoughtworks_cafeteria user=admin")
 
-#--connection for the welcome page
+#--connection for the welcome pagestatus
 
 @app.route('/')
 def login_page():
-    return render_template('creating_login_page.html')
+    return render_template('welcome_page.html')
 
 #connection for the customer login page
 @app.route('/customer_page')
 def customer_page():
-    return render_template('creating_customer_login.html')
+    return render_template('customer_login_page.html')
+
+
 
 #checking the user_id within the database and navigate the the menu page
+@app.route('/post_data', methods=['POST'])
+def get_page():
+    post_data(connection, request.form)
+    return render_template('detail_page.html', shared=request.form)
+
+
 @app.route('/employee', methods=['POST'])
 def menu_page():
     post_data(connection, request.form)
     return render_template('menu_page.html', shared=request.form)
 
-#connection for the juice item page
-@app.route('/juice_bevarage')
-def list_of_fruits():
-    return render_template('add_cart_fruits.html')
-
-#connection for the menu hot item page
-@app.route ('/hot_bevarage')
-def list_of_hots():
-    return render_template('add_cart_for_hot.html')
-
-#checking the vendor login within the database
-@app.route ('/menu_page', methods=['POST'])
-def get_vendor_data():
-    post_data(connection, request.form)
-    return render_template('vendor_items_availability_and_report.html', shared=request.form)
-
-#get the ordered items from cart
-@app.route('/juice_beverage')
-def get_order_item_for_juice():
-    post_data(connection, request.form)
-    return render_template('final_page.html', shared=request.form)
-
-#get the ordered  items from cart
-def get_order_item_for_hot():
-    post_data(connection, request.form)
-    return render_template('final_page.html', shared=request.form)
+# #connection for the juice item page
+# @app.route('/juice_bevarage')
+# def list_of_fruits():
+#     return render_template('add_cart_fruits.html')
+#
+# #connection for the menu hot item page
+# @app.route ('/hot_bevarage')
+# def list_of_hots():
+#     return render_template('add_cart_for_hot.html')
+#
+# #checking the vendor login within the database
+# @app.route ('/menu_page', methods=['POST'])
+# def get_vendor_data():
+#     post_data(connection, request.form)
+#     return render_template('vendor_items_availability_and_report.html', shared=request.form)
+#
+# #get the ordered items from cart
+# @app.route('/juice_beverage')
+# def get_order_item_for_juice():
+#     post_data(connection, request.form)
+#     return render_template('final_page.html', shared=request.form)
+#
+# #get the ordered  items from cart
+# def get_order_item_for_hot():
+#     post_data(connection, request.form)
+#     return render_template('final_page.html', shared=request.form)
 
 if __name__ == '__main__':
     app.run()
