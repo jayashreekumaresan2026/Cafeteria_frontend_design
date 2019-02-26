c2b96340fcae628235c21553cdb30c8245af8782
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-27 00:18:44 +0530

    added query to insert available items

--- cafeteria_menu.py
+++ cafeteria_menu.py
@@ -1,35 +1,43 @@
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from app import connection
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    row = database_connect()
-    return render_template("jinja.html", items=row)
+def cold():
+    rows = database_connection_cold()
+    items = []
+    for row in rows:
+        items.append(row[0])
+    return render_template("jinja_cold.html", items=items)
 
 
-def database_connect():
-    try:
-        print("try is running")
-        connection = psycopg2.connect(user="admin", host="127.0.0.1", port="5432",
-                                      database="thoughtworks_cafeteria")
-        cursor = connection.cursor()
-        cursor.execute("select  item_name from menu_item where ref_id= 'cb1'")
-        record = cursor.fetchall()
-
-        return record
+@app.route('/submission', methods=['POST'])
+def menu_list_cold():
+    return database_connection_list_cold(connection, request.form)
 
 
-    except (Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL", error)
+def database_connection_list_cold(connection, user_data):
+    cursor = connection.cursor()
+    d = dict(user_data)
+    for key in d:
+        cursor.execute("update menu_item set available = 1  where item_name= value(%s); ", (print(key),))
+    connection.commit()
+    cursor.close()
 
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+
+def database_connection_cold():
+    print("try is running")
+    connection = psycopg2.connect(user="admin", host="127.0.0.1", port="5432",
+                                  database="thoughtworks_cafeteria")
+    cursor = connection.cursor()
+    cursor.execute("select  item_name from menu_item where ref_id= 'cb1'")
+    record = cursor.fetchall()
+
+    return record
 
 
 if __name__ == '__main__':
