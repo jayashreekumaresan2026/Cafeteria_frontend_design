4abadc3662d0d11cf2032b4e40702cac3354ea62
Admin <admin@Intern6-MacBook-Pro.local>
2019-02-28 18:00:22 +0530

    added query for hot vendor

--- app.py
+++ app.py
@@ -20,21 +20,38 @@ def customer_page_login():
     return render_template('customer_login_page.html')
 
 
-@app.route('/beverage_for_cold')
-def list_of_juices():
+@app.route('/vendor')
+def hot_items():
+    rows = database_selected_hot_items()
+    items = []
+    for row in rows:
+        items.append(row[0])
+    return render_template("jinja_cold.html", items=items)
+
+
+def database_selected_hot_items():
+    cursor = connection.cursor()
+    cursor.execute("select  item_name from item_menu where ref_id ='222'")
+    record = cursor.fetchall()
+
+    return record
+
+
+@app.route('/menu_page_for_cold_items')
+def display_available_cold_items():
     return render_template('list_of_fruits.html')
 
 
-@app.route('/beverage_for_hot')
-def list_of_hots():
-    return render_template('list_of_hot.html')
+@app.route('/employee', methods=['POST'])
+def menu_pages_requested():
+    return post_data_customer(connection, request.form)
 
 
 # checking validation for the employee to move further process
-def post_data_customer(connection, data):
+def post_data_customer(connection, user_data):
     cursor = connection.cursor()
     cursor.execute("select employee_id from employee_details where employee_id = %(emp_id)s ",
-                   {'emp_id': data['id']}, )
+                   {'emp_id': user_data['id']}, )
     returned_rows = cursor.fetchall()
     cursor.close()
 
@@ -44,81 +61,73 @@ def post_data_customer(connection, data):
         return render_template('menu_page.html')
 
 
-@app.route('/employee', methods=['POST'])
-def menu_pages_requested():
-    return post_data_customer(connection, request.form)
-
-
 @app.route('/vendor_page')
 def vendor_page_login():
-    return render_template('vendor_login_page.html')
+    return render_template('vendor_login_pag.html')
 
 
 @app.route('/vendor_login', methods=['POST'])
-def item_pages_requested():
+def vendor_login_pages_requested():
     return post_data_vendor(connection, request.form)
 
 
-def post_data_vendor(connection, vendor_data):
+@app.route('/juice_world')
+def juice_login():
+    return render_template("vendor_login_page.html", name='111')
+
+
+@app.route('/madras_cafe')
+def hot_login():
+    return render_template("vendor_login_page.html", name='222')
+
+
+def post_data_vendor(connection, user_data):
     cursor = connection.cursor()
     cursor.execute(
-        "select vendor_id  from vendor_details where vendor_password = %(vendor_id)s ",
-        {'vendor_id': vendor_data['id']}, )
+        "select vendor_id,vendor_password  from vendor_detail where (vendor_id = %(vendor_id)s AND vendor_password=%(vendor_password)s )",
+        {'vendor_id': user_data['id'], 'vendor_password': user_data['psw']}, )
 
     returned_rows = cursor.fetchall()
     cursor.close()
     if len(returned_rows) == 0:
         return render_template('welcome_page.html')
     else:
-        return render_template('jinja_cold.html')
+
+        return render_template('vendor_items_availability_and_report.html', vendorname=user_data['vendorname'])
 
 
-@app.route('/beverage_for_cold')
-def cold_items():
-    rows = database_selected_cold_items()
+@app.route('/available_item')
+def cold_item():
+    rows = database_selected_cold_items(connection, request.query_string)
     items = []
     for row in rows:
         items.append(row[0])
     return render_template("jinja_cold.html", items=items)
 
 
-@app.route('/submission', methods=['POST'])
-def menu_list_cold():
-    return database_connection_list_cold(connection, request.form)
-
-
-@app.route('/beverage_for_hot' )
-def show_items_to_menu():
-    rows = database_connection_display()
-    items = []
-    for row in rows:
-        items.append(row[0])
-    return render_template("jinja_menu.html", items=items)
-
-
-def database_selected_cold_items():
+def database_selected_cold_items(connection, user_data):
     cursor = connection.cursor()
-    cursor.execute("select  item_name from menu_item where ref_id= 'cb1'")
+    query = "select  item_name from item_menu where ref_id =%s;"
+    cursor.execute(query, (int(user_data.decode().split('=')[1]),))
     record = cursor.fetchall()
 
     return record
 
 
-def database_connection_list_cold(connection, user_data):
+@app.route('/submission', methods=['POST'])
+def list_of_all_cold_items():
+    return set_list_of_in_database(connection, request.form)
+
+
+def set_list_of_in_database(connection, user_data):
     cursor = connection.cursor()
     array = tuple(user_data.keys())
-    sql_data = "update menu_item set available = 10 WHERE item_name  IN %s"
+    sql_update = "update item_menu set availability = 'no'"
+    sql_data = "update item_menu set availability = 'yes' WHERE item_name  IN %s"
+    cursor.execute(sql_update)
     cursor.execute(sql_data, (array,))
     connection.commit()
     cursor.close()
-
-
-def database_connection_display():
-    cursor = connection.cursor()
-    cursor.execute("select  item_name from menu_item where available= 10")
-    record_data = cursor.fetchall()
-
-    return record_data
 
 
 if __name__ == '__main__':
